d2984b169a4d31459e97e7b8d015de15c78d860c
Keith Gregory <kgregory@chariotsolutions.com>
2013-02-05 08:52:20 -0500

    change Cleaner option descriptions to active voice

--- app-cleaner/src/main/java/com/kdgregory/pomutil/cleaner/CommandLine.java
+++ app-cleaner/src/main/java/com/kdgregory/pomutil/cleaner/CommandLine.java
@@ -35,38 +35,33 @@ extends SimpleCLIParser
             new OptionDefinition(
                     Options.ORGANIZE_POM,
                     "--organizePom", "--noOrganizePom", false,
-                    "Restructures the entire POM to follow the order in the Maven documantion."
+                    "Restructure the entire POM to follow the order in the Maven documentation."
                     + " As a side-effect, removes any comments."),
             new OptionDefinition(
                     Options.PRETTY_PRINT,
                     "--prettyPrint", "--noPrettyPrint", true,
-                    "Pretty-prints the cleaned POM. By default, indentation is four spaces;"
+                    "Pretty-print the cleaned POM. By default, indentation is four spaces;"
                     + " you can control this with \"--prettyPrint=VALUE\"."),
-            new OptionDefinition(
-                    Options.ORGANIZE_POM,
-                    "--organizePom", "--noOrganizePom", false,
-                    "Restructures the entire POM to follow the order in the Maven documantion."
-                    + " As a side-effect, removes any comments."),
             new OptionDefinition(
                     Options.COMMON_PROPS,
                     "--commonProps", "--noCommonProps", true,
-                    "Inserts common build properties (such as source encoding) if not already present."),
+                    "Insert common build properties (such as source encoding) if not already present."),
             new OptionDefinition(
                     Options.VERSION_PROPS,
                     "--versionProps", "--noVersionProps", true,
-                    "Replaces hardcoded dependency versions with properties. These properties normally"
+                    "Replace hardcoded dependency versions with properties. These properties normally"
                     + " take the form \"GROUPID.version\"; where there are multiple artifacts with the"
                     + " same group but different versions, they take the form \"GROUPID.ARTIFACTID.version\"."),
             new OptionDefinition(
                     Options.VP_REPLACE_EXISTING,
                     "--replaceExistingProps", "--noReplaceExistingProps", false,
-                    "Replaces existing properties used as dependency versions (assures that all"
+                    "Replace existing properties used as dependency versions (assures that all"
                     + " version properties follow same form). Do not use if you inherit properties"
                     + " from a parent POM"),
             new OptionDefinition(
                     Options.VP_CONVERT_PLUGINS,
                     "--convertPluginVersions", "--noConvertPluginVersions", true,
-                    "Creates properties for plugins as well as normal dependencies. These properties"
+                    "Create properties for plugins as well as normal dependencies. These properties"
                     + " take the form \"plugin.ARTIFACTID.version\"."),
             new OptionDefinition(
                     Options.VP_ARTIFACT_ID,
@@ -76,12 +71,12 @@ extends SimpleCLIParser
             new OptionDefinition(
                     Options.DEPENDENCY_NORMALIZE,
                     "--dependencyNormalize", "--noDependencyNormalize", true,
-                    "Ensures that the children of a <dependency> element follow the order shown"
+                    "Ensure that the children of a <dependency> element follow the order shown"
                     + " in the Maven POM documentation."),
             new OptionDefinition(
                     Options.DEPENDENCY_SORT,
                     "--dependencySort", "--noDependencySort", true,
-                    "Sorts <dependency> elements by groupId and artifactId."),
+                    "Sort <dependency> elements by groupId and artifactId."),
             new OptionDefinition(
                     Options.DEPENDENCY_SORT_BY_SCOPE,
                     "--groupDependenciesByScope", "--noGroupDependenciesByScope", false,
@@ -89,7 +84,7 @@ extends SimpleCLIParser
             new OptionDefinition(
                     Options.PLUGIN_NORMALIZE,
                     "--pluginNormalize", "--noPluginNormalize", true,
-                    "Ensures that the children of a <plugin> element follow the order shown"
+                    "Ensure that the children of a <plugin> element follow the order shown"
                     + " in the Maven POM documentation, and adds an explicit <groupId> if"
                     + " the specification is relying on the default.")
             );

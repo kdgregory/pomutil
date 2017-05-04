19a138230e0a8d64140ae121b97f20d9e19737b9
Keith Gregory <contact@kdgregory.com>
2017-05-04 08:17:14 -0400

    add test to verify that unexpected version is left alone

--- app-version/src/test/java/com/kdgregory/pomutil/version/TestVersionUpdates.java
+++ app-version/src/test/java/com/kdgregory/pomutil/version/TestVersionUpdates.java
@@ -1,6 +1,7 @@
 package com.kdgregory.pomutil.version;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +10,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import net.sf.kdgcommons.io.IOUtil;
+import net.sf.practicalxml.DomUtil;
+import net.sf.practicalxml.OutputUtil;
 
+import com.kdgregory.pomutil.util.PomPaths;
 import com.kdgregory.pomutil.util.PomWrapper;
 
 
 public class TestVersionUpdates
 {
+    private static String TESTPOM_VERSION = "1.0.0";
+
+
     private List<String> createTestPoms(String resourceName, int numCopies)
     throws IOException
     {
@@ -33,11 +40,10 @@ public class TestVersionUpdates
     @Test
     public void testBasicOperation() throws Exception
     {
-        String oldVersion = "1.0.0";
-        String newVersion = "1.1.0";
-
         List<String> poms = createTestPoms("basepom.xml", 2);
-        new VersionUpdater(oldVersion, newVersion, false, poms).run();
+
+        String newVersion = "1.1.0";
+        new VersionUpdater(TESTPOM_VERSION, newVersion, false, poms).run();
 
         for (String pom : poms)
         {
@@ -45,5 +51,31 @@ public class TestVersionUpdates
             assertEquals(newVersion, wrapped.getGAV().getVersion());
         }
     }
+
+
+    @Test
+    public void testOnlyMatchingVersionsUpdated() throws Exception
+    {
+        List<String> poms = createTestPoms("basepom.xml", 2);
+        File pomToUpdate = new File(poms.get(0));
+        File pomToIgnore = new File(poms.get(1));
+
+        String testVersion = "1.2.3-SNAPSHOT";
+        PomWrapper updateWrapper = new PomWrapper(pomToIgnore);
+        DomUtil.setText(updateWrapper.selectElement(PomPaths.PROJECT_VERSION), testVersion);
+        FileOutputStream out = new FileOutputStream(pomToIgnore);
+        OutputUtil.compactStream(updateWrapper.getDom(), out);
+        out.close();
+
+        String newVersion = "1.0.1-SNAPSHOT";
+        new VersionUpdater(TESTPOM_VERSION, newVersion, false, poms).run();
+
+        PomWrapper check1 = new PomWrapper(pomToUpdate);
+        assertEquals("expected updated version", newVersion, check1.getGAV().getVersion());
+
+        PomWrapper check2 = new PomWrapper(pomToIgnore);
+        assertEquals("expected original version", testVersion, check2.getGAV().getVersion());
+    }
+
 
 }
